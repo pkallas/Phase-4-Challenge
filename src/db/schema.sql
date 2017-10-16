59dcab43bccc674c6ca55511f13af8f7aca89cb6
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-10-16 10:31:49 -0700

    Updated schema with users and reviews tables

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,5 +1,25 @@
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
-  id SERIAL,
+  id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   artist VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) UNIQUE NOT NULL,
+  profile_pic VARCHAR(255),
+  date_joined TIMESTAMP
+);
+
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
+  album_id INTEGER REFERENCES albums (id) ON DELETE CASCADE,
+  date_written TIMESTAMP,
+  description TEXT
 );
