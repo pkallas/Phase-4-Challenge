7e6655e983f0b145843238050531e4d10fc88f72
pkallas <pkallas@patricks-mbp.lan>
2017-10-16 13:13:46 -0700

    Wrote more functions for reviews db

--- src/models/db/reviews.js
+++ src/models/db/reviews.js
@@ -4,10 +4,31 @@ const getThreeMostRecent = () => {
   return db.query(`SELECT description, date_written, users.name, albums.title
     FROM reviews JOIN users ON users.id = reviews.user_id
     JOIN albums ON albums.id = reviews.album_id
-    ORDER BY date_written DESC`)
-    .catch(error => { throw error });
+    ORDER BY date_written DESC
+    LIMIT 3`)
+  .catch(error => { throw error });
+};
+
+const getAllForOneAlbum = (albumID) => {
+  return db.query(`SELECT description, date_written, users.name, albums.title
+    FROM reviews JOIN users ON users.id = reviews.user_id
+    JOIN albums ON albums.id = reviews.album_id
+    WHERE reviews.album_id = $1
+    ORDER BY date_written DESC`, [albumID])
+  .catch(error => { throw error });
+};
+
+const getAllForOneUser = (userID) => {
+  return db.query(`SELECT description, date_written, albums.title
+    FROM reviews JOIN users ON users.id = reviews.user_id
+    JOIN albums ON albums.id = reviews.album_id
+    WHERE reviews.user_id = $1
+    ORDER BY date_written DESC`, [userID])
+  .catch(error => { throw error });
 };
 
 module.exports = {
   getThreeMostRecent,
+  getAllForOneAlbum,
+  getAllForOneUser,
 };
