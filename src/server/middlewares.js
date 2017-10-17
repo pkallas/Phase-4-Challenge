be9966561e0e7cc711914875db4a1743bc5ac671
pkallas <pkallas@patricks-mbp.lan>
2017-10-16 17:27:00 -0700

    Created function to set default response locals in middlewares

---
+++ src/server/middlewares.js
@@ -0,0 +1,28 @@
+const setDefaultResponseLocals = (request, response, next) => {
+  response.locals.reviews = undefined;
+  response.locals.reviewAuthor = undefined;
+  if (request.session.userID) {
+    response.locals.session = true;
+    response.locals.userID = request.session.userID;
+    next();
+  } else {
+    response.locals.session = false;
+    response.locals.userID = undefined;
+    next();
+  };
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error);
+  response.status(500).render('error');
+};
+
+const notFound = (req, res) => {
+  res.status(404).render('not_found');
+};
+
+module.exports = {
+  setDefaultResponseLocals,
+  notFound,
+  errorHandler,
+};
