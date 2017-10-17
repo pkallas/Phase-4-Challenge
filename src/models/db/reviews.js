8e30b79618d716cb40cbbb913edf8495b39d4470
pkallas <pkallas@patricks-mbp.lan>
2017-10-16 17:15:56 -0700

    Changed review db functions to return all columns from database

--- src/models/db/reviews.js
+++ src/models/db/reviews.js
@@ -1,8 +1,8 @@
 const db = require('./db');
 
 const getThreeMostRecent = () => {
-  return db.query(`SELECT description, date_written, users.name, albums.title
-    FROM reviews JOIN users ON users.id = reviews.user_id
+  return db.query(`SELECT * FROM reviews
+    JOIN users ON users.id = reviews.user_id
     JOIN albums ON albums.id = reviews.album_id
     ORDER BY date_written DESC
     LIMIT 3`)
@@ -10,8 +10,8 @@ const getThreeMostRecent = () => {
 };
 
 const getAllForOneAlbum = (albumID) => {
-  return db.query(`SELECT description, date_written, users.name, albums.title
-    FROM reviews JOIN users ON users.id = reviews.user_id
+  return db.query(`SELECT * FROM reviews
+    JOIN users ON users.id = reviews.user_id
     JOIN albums ON albums.id = reviews.album_id
     WHERE reviews.album_id = $1
     ORDER BY date_written DESC`, [albumID])
@@ -19,8 +19,8 @@ const getAllForOneAlbum = (albumID) => {
 };
 
 const getAllForOneUser = (userID) => {
-  return db.query(`SELECT description, date_written, albums.title
-    FROM reviews JOIN users ON users.id = reviews.user_id
+  return db.query(`SELECT * FROM reviews
+    JOIN users ON users.id = reviews.user_id
     JOIN albums ON albums.id = reviews.album_id
     WHERE reviews.user_id = $1
     ORDER BY date_written DESC`, [userID])
